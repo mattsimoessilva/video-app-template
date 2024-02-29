82367d17a26eca44ec6a3ddf2224c505b676fc7c
Matheus Simões da Silva <matheussimoesdasilva2002@gmail.com>
2024-02-29 14:14:26 -0300

    Making the video grid

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8acccc536f36e9629045a8c688bf273c2fa674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08028f058c434a117f775aedfb7e44f905b3226f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
